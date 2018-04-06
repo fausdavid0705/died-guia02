6d1749a08fd80880f763967476f812a20b53d25e
fausdavid0705 <faus.david.7@gmail.com>
2018-04-05 23:47:23 -0300

    version inicial

---
+++ src/died/guias/guia02/app/Hola2.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package died.guias.guia02.app;
+
+/**
+ * @author Faus
+ *
+ */
+public class Hola2 {
+
+	/**
+	* @param args
+	*/
+	public static void main(String[] args) {
+	System.out.println("el tiempo actual en milis es "+
+	System.currentTimeMillis());
+	}
+}
